22de327b25b695b0788b0303fa0e8ed96427aaa5
Haragos99 <geri.haragos@gmail.com>
2025-05-05 23:35:13 +0200

    create scripts path

--- MyMayaPlugin/scripts/Unload.py
+++ MyMayaPlugin/scripts/Unload.py
@@ -6,8 +6,10 @@ def unload_plugin(plugin_name, port=7001):
             cmd = f'import maya.cmds as cmds; cmds.unloadPlugin("{plugin_name}")\n'
             s.sendall(cmd.encode('utf-8'))
             print(f"Sent unload command for plugin: {plugin_name}")
-    except Exception as e:
-        print(f"Failed to send command: {e}")
+    except Exception as ex:
+        print(f"Failed to send command: {ex}")
 
-# Replace 'MyMayaPlugin' with your plugin's name
+
+
+# Load the Plugin 
 unload_plugin('MyMayaPlugin')
