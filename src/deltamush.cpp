8c89b890f85e703f1410715b95727a2f08e6cae7
Haragos99 <geri.haragos@gmail.com>
2025-07-31 20:34:12 +0200

    add Delta Mush Calculate Deformation

--- src/deltamush.cpp
+++ src/deltamush.cpp
@@ -95,12 +95,7 @@ void DeltaMush::CalculateDelta()
 		bitangent.normalize();
 
 		// Construct transformation matrix R
-		MMatrix R;
-		R[0][0] = tangent.x;   R[0][1] = bitangent.x;   R[0][2] = normal.x;   R[0][3] = p.x;
-		R[1][0] = tangent.y;   R[1][1] = bitangent.y;   R[1][2] = normal.y;   R[1][3] = p.y;
-		R[2][0] = tangent.z;   R[2][1] = bitangent.z;   R[2][2] = normal.z;   R[2][3] = p.z;
-		R[3][0] = 0.0;         R[3][1] = 0.0;           R[3][2] = 0.0;        R[3][3] = 1.0;
-
+		MMatrix R = initMatrix(p, normal, tangent, bitangent);
 		// Invert the transformation matrix
 		MMatrix R_inv = R.inverse();
 
@@ -110,10 +105,71 @@ void DeltaMush::CalculateDelta()
 
 	}
 	MGlobal::displayInfo("Delta successfully.");
-
-
-
-
 	//m_mesh.recalculateNormals();
 	//m_mesh.updateMesh();
+}
+
+MMatrix initMatrix(MPoint point, MVector normal, MVector tangent, MVector bitangent)
+{
+	MMatrix M;
+	M[0][0] = tangent.x;   M[0][1] = bitangent.x;   M[0][2] = normal.x;   M[0][3] = point.x;
+	M[1][0] = tangent.y;   M[1][1] = bitangent.y;   M[1][2] = normal.y;   M[1][3] = point.y;
+	M[2][0] = tangent.z;   M[2][1] = bitangent.z;   M[2][2] = normal.z;   M[2][3] = point.z;
+	M[3][0] = 0.0;         M[3][1] = 0.0;           M[3][2] = 0.0;        M[3][3] = 1.0;
+
+	return M;
+}
+
+void DeltaMush::CalculateDeformation()
+{
+	auto smooth = smoothMesh(m_mesh, 10);
+	MStatus status;
+	auto vertIter = smooth.getVertexIterator(&status);
+	MPointArray deformedPoints;
+	for (; !vertIter->isDone(); vertIter->next())
+	{
+		int vertIndex = vertIter->index();
+		MPoint point = vertIter->position(MSpace::kWorld, &status);
+		if (status != MS::kSuccess)
+		{
+			MGlobal::displayError("Failed to get vertex position.");
+			continue;
+		}
+		// Retrieve vertex normal
+		MVector normal;
+		status = vertIter->getNormal(normal, MSpace::kWorld);
+		if (status != MS::kSuccess)
+		{
+			MGlobal::displayError("Failed to get vertex normal.");
+			continue;
+		}
+		normal.normalize();
+
+		MIntArray connectedVertices;
+		status = vertIter->getConnectedVertices(connectedVertices);
+		if (status != MS::kSuccess || connectedVertices.length() == 0) {
+			MGlobal::displayError("Failed to get connected vertices.");
+			continue;
+		}
+
+		MPoint p1 = smooth.getNextPoint(connectedVertices[0]);
+
+		MVector edge = p1 - point;
+
+		// Compute tangent vector by removing the component along the normal
+		MVector tangent = edge - (edge * normal) * normal;
+		tangent.normalize();
+
+		// Compute bitangent as cross product of normal and tangent
+		MVector bitangent = normal ^ tangent;
+		bitangent.normalize();
+
+		// Construct transformation matrix C
+		MMatrix C = initMatrix(point, normal, tangent, bitangent);
+		MPoint defompoint = C * deltas[vertIndex];
+		deformedPoints.append(defompoint);
+
+	}
+	m_mesh.setVertices(deformedPoints);
+	m_mesh.updateMesh();
 }
