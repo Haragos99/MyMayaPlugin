f955809baaa023441d97a8db9e36396b1d7dd018
Haragos99 <geri.haragos@gmail.com>
2025-09-02 18:55:30 +0200

    add deltamushnode

---
+++ src/deltamushnode.cpp
@@ -0,0 +1,50 @@
+#include "deltamushnode.h"
+
+MTypeId DeltaMushNode::id(0x00123456);
+
+std::shared_ptr<DeltaMush> DeltaMushNode::g_deltamushCache = nullptr;
+
+
+MStatus DeltaMushNode::deform(MDataBlock& data,
+    MItGeometry& itGeo,
+    const MMatrix& localToWorldMatrix,
+    unsigned int geomIndex)
+{
+
+    
+    MStatus status;
+    MArrayDataHandle inputArray = data.inputArrayValue(input, &status);
+    inputArray.jumpToElement(geomIndex);
+
+    MDataHandle inputGeomData = inputArray.inputValue().child(inputGeom);
+    MObject meshObj = inputGeomData.asMesh();  // or asMeshTransformed()
+    MFnMesh fnMesh(meshObj, &status);
+    MGlobal::displayInfo("NODE ");
+    if (g_deltamushCache != nullptr)
+    {
+        MGlobal::displayInfo(fnMesh.name());
+        
+    }
+    else
+    {
+        MGlobal::displayInfo("Bad ");
+    }
+
+    // Iterate over skinned geometry
+    for (; !itGeo.isDone(); itGeo.next())
+    {
+        unsigned int vid = itGeo.index();
+
+        // 1. Get the CURRENT vertex position (after skinning)
+        MPoint skinnedPos = itGeo.position();
+        MPoint avgPos(1, 1, 1);
+        skinnedPos += avgPos;
+       // itGeo.setPosition(avgPos);
+        break;
+    }
+    //g_deltamushCache->move();
+
+
+
+    return MS::kSuccess;
+}
---
+++ src/deltamushnode.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "framework.h"
+#include "deltamush.h"
+
+class DeltaMushNode : public MPxDeformerNode
+{
+public:
+    static void* creator() { return new DeltaMushNode; }
+    static MStatus initialize() { return MS::kSuccess; }
+    MStatus deform(MDataBlock& data,
+        MItGeometry& itGeo,
+        const MMatrix& localToWorldMatrix,
+        unsigned int geomIndex) override;
+
+    static MTypeId id;
+    static  std::shared_ptr<DeltaMush> g_deltamushCache;
+};
